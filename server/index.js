b3ef1820ad3b15554eb4ddbe110b24326b3bf90c
Samarth Gupta <samarthgupta1202@gmail.com>
2023-01-26 21:48:48 +0530

    server/index.js updated

--- server/index.js
+++ server/index.js
@@ -3,3 +3,18 @@ import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = "mongodb+srv://samarth:samarth123@memories-project.51tfjmr.mongodb.net/?retryWrites=true&w=majority" //this is to connect to the cluster
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => { console.log(`Serve running on port : ${PORT}`); }))
+    .catch((error) => { console.log(error.message); });
+
+mongoose.set("useFindAndModify", false) //this makes sure we dont get any warnings in the console
