09d046019bbabebe2a5b0eb1fbfb9bd7127cf223
Samarth Gupta <samarthgupta1202@gmail.com>
2023-01-27 22:22:41 +0530

    try and catch added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,24 @@
 import PostMessage from "../models/postMessage.js"
 
-export const getPosts = (req, res) => {
-    res.send("This works!!")
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find() //since this takes time, we make it async
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+
+    }
 }
 
-export const createPost = (req, res) => {
-    res.send("Post creation")
+export const createPost = async (req, res) => {
+    const post = req.body
+    const newPost = new postMessage(post)
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
 }
 
 //export is used when we have multiple modules/variables to export. We import it with {name}
